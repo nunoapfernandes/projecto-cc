d2c2e051493568bee7280667378dd3c7ba115db6
Nuno Fernandes <nunofernandes@guimaform.pt>
2017-04-06 12:05:53 +0100

    Definicoes IntelliJ iniciadas

---
+++ reverse-proxy/src/back_end_client/Client.java
@@ -0,0 +1,9 @@
+package back_end_client;
+
+
+public class Client {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ reverse-proxy/src/front_end_server/Server.java
@@ -0,0 +1,11 @@
+package front_end_server;
+
+
+public class Server {
+
+    private Server(){};
+
+    public static void main(String[] args){
+
+    }
+}
