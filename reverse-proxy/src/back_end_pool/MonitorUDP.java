a87d2ae6dbe7904b2115a47dd45961963841c7c6
ukalahs <nunofernandes313@gmail.com>
2017-04-20 09:58:17 +0100

    Monitor UDP actualizado

--- reverse-proxy/src/back_end_pool/MonitorUDP.java
+++ reverse-proxy/src/back_end_pool/MonitorUDP.java
@@ -13,26 +13,24 @@ public class MonitorUDP {
 
     public static void main(String[] args) throws IOException, TypeNotFoundException {
 
-        InetAddress MyIPAddress = InetAddress.getLocalHost();
+        InetAddress MyIPAddress = InetAddress.getByName("172.16.82.129");
         InetAddress ServerIPAddress = InetAddress.getByName("192.168.1.172");
-        System.out.println(MyIPAddress.toString());
         PDUManager message = new PDUManager(1, MyIPAddress);
 
-        DatagramSocket clientSocket = new DatagramSocket();
+        DatagramSocket clientSocket = new DatagramSocket(5555);
         DatagramPacket sendPacket = new DatagramPacket(message.buildPDU(), message.buildPDU().length, ServerIPAddress, 5555);
         clientSocket.send(sendPacket);
 
         while (true) {
             //RECEBE MENSAGEM E CONSTROI PDUManager
             PDUManager messageReceived = new PDUManager();
-            byte[] receiveData = messageReceived.buildPDU();
+            byte[] receiveData = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             messageReceived.generatePDU(receiveData);
 
             //ENVIA UM PDUManager DO TIPO ANSWER, COM O PRÓPRIO IP E O CONTADOR DA ULTIMA MENSAGEM QUE RECEBEU
             message = new PDUManager(3, MyIPAddress, messageReceived.getCounter());
-            clientSocket = new DatagramSocket();
             sendPacket = new DatagramPacket(message.buildPDU(), message.buildPDU().length, ServerIPAddress, 5555);
             clientSocket.send(sendPacket);
 
